9c901a6c03ce4ac63c007a0fc80ac6bce91930e9
Ankita Jaiswal <jaisankita31@gmail.com>
2024-08-18 10:06:43 +0200

    HelpView implemented

---
+++ GroceryLivs/Models/HelpItem.swift
@@ -0,0 +1,14 @@
+//
+//  HelpItem.swift
+//  GroceryLivs
+//
+//  Created by Ankita Jaiswal on 17/08/24.
+//
+
+import Foundation
+
+struct HelpItem {
+    let question: String
+    let answer: String
+    var isExpanded: Bool = false
+}
